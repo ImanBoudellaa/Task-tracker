8682ad57124fac613071b92bf9b329d84cdfbe81
ImanBoudellaa <boudellaiman@gmail.com>
2021-06-28 15:33:07 +0200

    border if task.reminder is set true

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
   const [tasks, setTasks] = useState([{
     id: 1,
     text: 'Doktor',
-    day: 'Feb 5th at 2:30',
+    day: '2021-06-21 u 12:30',
     reminder: false
   }, {
     id: 2,
     text: 'Masaza',
-    day: 'Feb 6th at 2:30',
+    day: '2021-06-30 u 16:30',
     reminder: true
   },
 
@@ -18,10 +18,16 @@ function App() {
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
+
+
+  const reminder = (id) => {
+    setTasks(tasks.map((task)=>task.id==id ?{...task,reminder:!task.reminder}:task))
+    
+  }
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No tasks'}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={reminder} /> : 'No tasks'}
     </div>
   );
 }
